d64ec2a2abe3721b1a1807ba2e7fe26d57edac4f
bhuvaneshwari29 <bhuvans.raja@gmail.com>
2021-01-23 12:15:18 +0100

    Changed the simplied formula and constants to let

--- javascript/javascript1/week1/EzNamey.js
+++ javascript/javascript1/week1/EzNamey.js
@@ -1,6 +1,6 @@
 //startup name generator
-const firstWords = ['Easy','Awesome','Corporate','Company','Startup','Software','Information Technology','Cognizant','TCS','Infosys'];
-const secondWords = ['Red','blue','yellow', 'green', 'Violet','Circle','Square','Triangle','Rectangle', 'Oval'];
-const randomNumber = Math.floor(Math.random() * 10) + 0;
-const startUpName = firstWords[randomNumber] + ' ' + secondWords[randomNumber];
+let firstWords = ['Easy','Awesome','Corporate','Company','Startup','Software','Information Technology','Cognizant','TCS','Infosys'];
+let secondWords = ['Red','blue','yellow', 'green', 'Violet','Circle','Square','Triangle','Rectangle', 'Oval'];
+let randomNumber = Math.floor(Math.random()*firstWords.length);
+let startUpName = firstWords[randomNumber] + ' ' + secondWords[randomNumber];
 console.log('The startup: ' + startUpName + ' ' + 'contains' + ' ' + startUpName.length + ' ' + 'characters');
