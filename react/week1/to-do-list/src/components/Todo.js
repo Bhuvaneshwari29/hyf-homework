79d1bea7e1b2e48508045f6a792569a3b82a5003
bhuvaneshwari29 <bhuvans.raja@gmail.com>
2021-05-21 14:01:57 +0200

    React week1 homework

---
+++ react/week1/to-do-list/src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Todo(props) {
+  return (
+    <ul>
+      <li className="todo-work">
+        <span>{props.item.id}</span>
+        <span>{props.item.work}</span>
+        <span>{props.item.bydate}</span>
+      </li>
+    </ul>
+  );
+}
+
+export default Todo;
