fe66a893e76565544b868ca9a31c8ffed25880cb
bhuvaneshwari29 <bhuvans.raja@gmail.com>
2021-05-31 18:24:52 +0200

    Feedback implemented

--- react/week1/to-do-list/src/components/Todo.js
+++ react/week1/to-do-list/src/components/Todo.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Todo(props) {
   return (
-    <ul>
+    < >
       <li className="todo-work">
         <span>{props.item.id}</span>
         <span>{props.item.work}</span>
         <span>{props.item.bydate}</span>
       </li>
-    </ul>
+    </>
   );
 }
 
